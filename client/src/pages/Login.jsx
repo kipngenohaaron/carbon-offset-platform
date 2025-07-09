7617635022e05c5780c0027f89a6fdb32480c934
kipngenohaaron <kipngenohaaron@gmail.com>
2025-07-09 18:20:57 +0300

    updating the frontend

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold text-green-600 mb-2">Login Page</h2>
+      <p>Coming soon: user authentication</p>
+    </div>
+  );
+}
+
+export default Login;
