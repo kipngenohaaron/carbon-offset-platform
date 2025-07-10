259eacd8be2c10d76685fec0d0744a82de601a07
kipngenohaaron <kipngenohaaron@gmail.com>
2025-07-10 09:05:36 +0300

    updating the routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Calculator from "./pages/Calculator";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 
