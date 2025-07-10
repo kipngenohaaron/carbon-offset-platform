b68ed61b09c8dca618f2d3f3d2d95ac0f56f2e31
kipngenohaaron <kipngenohaaron@gmail.com>
2025-07-10 12:26:31 +0300

    updating the models

--- server/models/trade.py
+++ server/models/trade.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Trade(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_email = db.Column(db.String(120), nullable=False)
-    credit_amount = db.Column(db.Float, nullable=False)
+    credit_amount = db.Column(db.Float, nullable=False)  # tonnes of CO₂
     price_per_tonne = db.Column(db.Float, nullable=False)
-    trade_type = db.Column(db.String(10), nullable=False)
+    trade_type = db.Column(db.String(10), nullable=False)  # 'buy' or 'sell'
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
