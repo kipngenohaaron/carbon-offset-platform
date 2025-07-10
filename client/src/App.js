35dcaeffd97ec8327942754807f5faea2239da80
kipngenohaaron <kipngenohaaron@gmail.com>
2025-07-10 09:05:12 +0300

    updating the routes

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/calculator" element={<Calculator />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </Router>
