c6c804e7736874ec6aee40d31eac0778b50caea4
kipngenohaaron <kipngenohaaron@gmail.com>
2025-07-10 10:10:44 +0300

    updating the models

--- server/models/database.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///carbon.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
