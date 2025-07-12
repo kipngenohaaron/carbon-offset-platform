0629de936cf20e3fc1553c29857f49909fd8d1a5
kipngenohaaron <kipngenohaaron@gmail.com>
2025-07-12 11:43:02 +0300

    updating backend

--- server/app.py
+++ server/app.py
@@ -6,12 +6,13 @@ from server.routes.calculator import calculator
 from server.routes.auth import auth
 
 from server.database import init_db, db
-from server.models import user, trade  # ensures model registration
+from server import models  # ✅ just register models, avoid name clash
 
 app = Flask(__name__)
 CORS(app)
 init_db(app)
 
+# Register route blueprints
 app.register_blueprint(trade)
 app.register_blueprint(calculator)
 app.register_blueprint(auth)
