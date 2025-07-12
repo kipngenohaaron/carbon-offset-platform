2cda16de4ebf09f774886d00d0b327528a05f7bb
kipngenohaaron <kipngenohaaron@gmail.com>
2025-07-12 11:30:06 +0300

    updating routes

--- server/models/trade.py
+++ server/models/trade.py
@@ -1,10 +1,10 @@
-from ..database import db
+from server.database import db
 from datetime import datetime
 
 class Trade(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_email = db.Column(db.String(120), nullable=False)
-    credit_amount = db.Column(db.Float, nullable=False)  # tonnes of CO₂
+    credit_amount = db.Column(db.Float, nullable=False)
     price_per_tonne = db.Column(db.Float, nullable=False)
     trade_type = db.Column(db.String(10), nullable=False)  # 'buy' or 'sell'
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
